93b2d458a889894814b1f56b6d0df12d723f61e6
Lucas Sproule <109782202+lsproule@users.noreply.github.com>
2026-09-15 09:03:29 +0700

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []        
+        for log in logs:
+            if log == "./":
+                continue
+            if log == "../":
+                if len(stack) >= 1:
+                    stack.pop()
+                continue
+            stack.append(log)
+        print(stack)
+        return len(stack)
